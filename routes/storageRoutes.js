d67cbf089391774ebfe67dfc9d5045d9d4efdf28
jettpbaker <jett.p.baker@gmail.com>
2025-03-03 14:54:08 +1000

    added functionality to preview uploaded file

--- routes/storageRoutes.js
+++ routes/storageRoutes.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import * as fileController from '../controllers/fileController.js'
+import { isAuthenticated } from '../middleware/auth.js'
 
 const router = express.Router()
+
+// Apply authentication middleware to all storage routes
+router.use(isAuthenticated)
 
 // Get routes
 router.get('/upload', fileController.getUpload)
 router.get('/download/:id', fileController.getDownload)
+router.get('/file/:id', fileController.getFile)
 
 // Post routes
 router.post('/upload', fileController.postUploadFile)
